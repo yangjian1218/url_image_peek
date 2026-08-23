4dad651d1fb136637ab259f5fff725aebf179e21
杨健 <yangjian@zj.tech>
2026-08-23 21:44:13 +0800

    fix: initialize ImagePeek app delegate

--- ImagePeek/AppShell/ImagePeekApp.swift
+++ ImagePeek/AppShell/ImagePeekApp.swift
@@ -2,7 +2,14 @@ import AppKit
 
 @main
 final class ImagePeekApp: NSObject, NSApplicationDelegate {
+    private static let appDelegate = ImagePeekApp()
     private var menuBarController: MenuBarController?
+
+    static func main() {
+        let application = NSApplication.shared
+        application.delegate = appDelegate
+        application.run()
+    }
 
     func applicationDidFinishLaunching(_ notification: Notification) {
         NSApp.setActivationPolicy(.accessory)
